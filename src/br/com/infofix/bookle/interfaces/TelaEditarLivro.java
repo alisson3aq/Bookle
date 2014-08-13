09d743d95dee69c51c534e1b5203885ca7d6b431
admin <admin@10.0.0.100>
2014-08-13 11:01:16 -0300

    Alteração de Preenchimento nos campos editar.

--- src/br/com/infofix/bookle/interfaces/TelaEditarLivro.java
+++ src/br/com/infofix/bookle/interfaces/TelaEditarLivro.java
@@ -33,8 +33,8 @@ public class TelaEditarLivro extends javax.swing.JInternalFrame {
     public TelaEditarLivro(String codlivro) {
         initComponents();
         telapesquisar.preencheComboBox("select * from tbdisciplina", "nomedisciplina", jcomboboxDisciplinas);
-        preencheCamposEditar(codlivro);
         setLimitTextFields();
+        preencheCamposEditar(codlivro);
     }
 
     /**
